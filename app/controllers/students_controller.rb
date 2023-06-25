f5501cbf025aa30719ac49c434c27521730b46de
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-06-25 21:07:35 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name].present?
+      @students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    else
+      @students = Student.all
+    end
+    render json: @students
   end
-
+          
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  
 end
